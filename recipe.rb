91c948caa17adc3e325c1b4d99d66cfbc1cc709c
TAC <tac@tac42.net>
2015-12-11 00:01:37 +0900

    Add docker

--- recipe.rb
+++ recipe.rb
@@ -77,3 +77,38 @@ execute 'chsh zsh' do
   user "root"
   command "chsh #{USER} -s /bin/zsh"
 end
+
+##########
+# Docker #
+##########
+packages = %W|docker.io|
+packages.each {|p| package p }
+
+execute "enable tab-completion of Docker commands in BASH" do
+  command <<-COMMANDS
+#!/bin/bash
+source /etc/bash_completion.d/docker.io
+  end
+end
+execute "install apt-transport-https" do
+  command <<-COMMANDS
+[ -e /usr/lib/apt/methods/https ] || {
+  apt-get update
+  apt-get install apt-transport-https
+}
+  COMMANDS
+  only_if "find /etc/bash_completion.d -name docker.io"
+end
+
+execute "install apt-transport-https" do
+  command <<-COMMANDS
+sudo sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
+sudo sh -c "echo deb http://get.docker.io/ubuntu docker main\ > /etc/apt/sources.list.d/docker.list"
+sudo apt-get update
+  COMMANDS
+end
+
+package 'lxc-docker' do
+  action :install
+  options("-y")
+end
